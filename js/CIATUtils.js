e174fd71a37b65b5b94c6cda010a5f86ea0908e4
JHerculesqz <JHercules_qz@qq.com>
2016-11-18 11:31:48 +0800

    讯飞臭狗屎，又修改API

--- js/CIATUtils.js
+++ js/CIATUtils.js
@@ -4,69 +4,70 @@
 function CATUtils(){
     //#region Fields
 
-    this.session = new IFlyIatSession({
-        'url'				  : 'wss://h5.openspeech.cn/iat.do',
-        'reconnection'       : true,
-        'reconnectionDelay'  : 30000,
-        'compress'           : 'speex'
-    });
+    this.session = undefined;
+    var mic_pressed = false;
 
     //#endregion
 
-    //#region dispose
+    //#region init
 
-    this.dispose = function(){
-        this.session.cancel();
+    this.init = function(oResCallback){
+        this.session = new IFlyIatSession({
+            "callback":{
+                "onResult": function (err, result) {
+                    /* 若回调的err为空或错误码为0，则会话成功，可提取识别结果进行显示*/
+                    if (err == null || err == undefined || err == 0) {
+                        if (result == '' || result == null){
+                            oResCallback("未获得识别结果...");
+                        }
+                        else{
+                            oResCallback(result);
+                        }
+                    }
+                    else {
+                        oResCallback('error code : ' + err + ", error description : " + result);
+                    }
+                    mic_pressed = false;
+                },
+                "onVolume": function (volume) {
+                    //console.log(volume);
+                },
+                "onError":function(){
+                    mic_pressed = false;
+                    console.log("onError");
+                },
+                "onProcess":function(status){
+                    console.log(status);
+                }
+            }
+        });
+
+        if(!this.session.isSupport()){
+            console.log("当前浏览器不支持！");
+        }
     };
 
     //#endregion
 
     //#region start
 
-    this.start = function(strAppId, strAppKey, oResCallback){
-        var ssb_param = {"grammar_list" : null, "params" : "appid=" + strAppId +
-            ",appidkey=" + strAppKey +
-            ", lang = sms, acous = anhui, aue=speex-wb;-1, " +
-            "usr = mkchen, ssm = 1, sub = iat, net_type = wifi, " +
-            "rse = utf8, ent =sms16k, rst = plain, auf  = audio/L16;rate=16000, " +
-            "vad_enable = 1, vad_timeout = 5000, " +
-            "vad_speech_tail = 500, compress = igzip"};
-        this.session.start(ssb_param, function(volume){
-            if(volume < 0){
-                console.log("麦克风启动失败...");
-            }
-            else{
-                console.log("麦克风正常获得声音...");
-            }
-        }, function (err, result){
-            if(err == null || err == undefined || err == 0){
-                if(result == '' || result == null){
-                    oResCallback("未获得识别结果...");
-                }
-                else{
-                    oResCallback(result);
-                }
-            }
-            else{
-                console.log(err);
-            }
-        }, function(message){
-            if(message == 'onStop')
-            {
-                console.log("录音停止");
-            } else if(message == 'onEnd')
-            {
-                console.log("会话结束");
-            }
-        });
-    };
-
-    //#endregion
-
-    //#region stop
-
-    this.stop = function(){
-        this.session.stop(null);
+    this.start = function(strAppId, strAppKey){
+        if(!mic_pressed){
+            var ssb_param = {
+                "grammar_list": null,
+                "params": "appid=581dcc9a,appidkey=5fd627de2b35a6fe, lang = sms, acous = anhui, aue=speex-wb;-1, usr = mkchen, ssm = 1, sub = iat, net_type = wifi, rse = utf8, ent =sms16k, rst = plain, auf  = audio/L16;rate=16000, vad_enable = 1, vad_timeout = 5000, vad_speech_tail = 500, compress = igzip"
+            };
+//            var ssb_param = {
+//                "grammar_list" : null,
+//                "params" : "appid=" + strAppId +
+//                    ",appidkey=" + strAppKey +
+//                    "lang = sms, acous = anhui, aue=speex-wb;-1, usr = mkchen, ssm = 1, sub = iat, net_type = wifi, rse = utf8, ent =sms16k, rst = plain, auf  = audio/L16;rate=16000, vad_enable = 1, vad_timeout = 5000, vad_speech_tail = 500, compress = igzip"};
+            this.session.start(ssb_param);
+            mic_pressed = true;
+        }
+        else{
+            this.session.stop();
+        }
     };
 
     //#endregion
